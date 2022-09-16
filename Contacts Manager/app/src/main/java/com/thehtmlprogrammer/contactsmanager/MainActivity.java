609428995377866f5166c2bf5005693eb07b2d6c
theHTMLprogrammer <stgupta9935@gmail.com>
2022-09-16 22:46:46 +0530

    Completed Vector Assets concepts

---
+++ Contacts Manager/app/src/main/java/com/thehtmlprogrammer/contactsmanager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.thehtmlprogrammer.contactsmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
